876c5404291f79276098a41c0722dc103bf44c2e
Mgonzalez06 <lisethgonzalez2000@gmail.com>
2024-07-26 22:33:50 -0600

    Deployment

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# drive-alerts
+# Drive Alerts
+
+Smart contract created to report traffic alerts along the road. This contract allows users to submit and view real-time traffic notifications, helping to keep drivers informed about road conditions and potential hazards
+
+# Create symlink
+
+mklink /D "C:\path\to\your\project\node_modules\drive-alerts-artifacts" "C:\path\to\drive-alerts\artifacts"
--- contracts/TrafficData.sol
+++ contracts/TrafficData.sol
@@ -2,30 +2,66 @@
 pragma solidity ^0.8.24;
 
 contract TrafficData {
+    struct Location {
+        int256 lat;
+        int256 lng;
+    }
+
     struct Report {
         address reporter;
-        string location;
+        Location location;
         string description;
         uint256 timestamp;
+        uint256 upvotes;
+        uint256 downvotes;
     }
 
     Report[] public reports;
+    mapping(address => int256) public points;
+    mapping(address => mapping(uint256 => bool)) public votes;
 
     function addReport(
-        string memory location,
-        string memory description
+        int256 _lat,
+        int256 _lng,
+        string memory _description
     ) public {
         reports.push(
             Report({
                 reporter: msg.sender,
-                location: location,
-                description: description,
-                timestamp: block.timestamp
+                location: Location({lat: _lat, lng: _lng}),
+                description: _description,
+                timestamp: block.timestamp,
+                upvotes: 0,
+                downvotes: 0
             })
         );
     }
 
     function getReports() public view returns (Report[] memory) {
         return reports;
+    }
+
+    function upvoteReport(uint256 _reportIndex) public {
+        require(_reportIndex < reports.length, "Invalid report index");
+        require(
+            !votes[msg.sender][_reportIndex],
+            "You have already voted on this report"
+        );
+
+        votes[msg.sender][_reportIndex] = true;
+        reports[_reportIndex].upvotes += 1;
+        points[reports[_reportIndex].reporter] += 1;
+    }
+
+    function downvoteReport(uint256 _reportIndex) public {
+        require(_reportIndex < reports.length, "Invalid report index");
+        require(
+            !votes[msg.sender][_reportIndex],
+            "You have already voted on this report"
+        );
+
+        votes[msg.sender][_reportIndex] = true;
+        reports[_reportIndex].downvotes += 1;
+        points[reports[_reportIndex].reporter] -= 1;
     }
 }
